ac04df3273168eaa5f0d6f4f2293a52a813907a7
rishabco <rishab.gangopadhyay@colorado.edu>
2015-04-27 00:56:21 -0600

    Class cpp file with functions declared

---
+++ Travel.cpp
@@ -0,0 +1,74 @@
+
+#include "Travel.h"
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+Travel::Travel()
+{
+
+}
+
+Travel::~Travel()
+{
+    //dtor
+}
+
+void Travel::addRoad(std::string v1, std::string v2, int weight)    //SET V1, V2 as roads
+{
+
+}
+
+void Travel::addCity(std::string cityName)
+{
+
+}
+
+void Travel::addHub(std::string cityName)  //GET RID OF THIS ONE
+{
+
+}
+
+void Travel::addPlanePath(std::string v1, std::string v2, int weight)
+{
+
+}
+
+
+void Travel::printPlanePaths()
+{
+
+}
+
+void Travel::printDistricts()
+{
+
+}
+
+void Travel::printCities()
+{
+
+}
+
+int Travel::calculateCost(string starting, string destination)
+{
+
+}
+
+int Travel::calculateTime(string starting, string destination)
+{
+//    cout << travelDistance << endl;
+}
+
+void Travel::shortestDistance(std::string starting, std::string destination)
+{
+
+}
+
+void Travel::findDistricts() //Based on isAirport BOOl
+{
+
+
+}
+
