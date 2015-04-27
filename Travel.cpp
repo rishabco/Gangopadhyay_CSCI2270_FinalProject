81661e5af0897bdfc9fae292bf0b1b867d923cf2
k <k@K>
2015-04-27 01:10:17 -0600

    Added the add functions for flight and ground paths..

--- Travel.cpp
+++ Travel.cpp
@@ -15,24 +15,113 @@ Travel::~Travel()
     //dtor
 }
 
-void Travel::addRoad(std::string v1, std::string v2, int weight)    //SET V1, V2 as roads
+void Travel::addRoad(std::string v1, std::string v2, int weight)
 {
-
+    for(int i = 0; i < vertices.size(); i++)
+    {
+        if(vertices[i].name == v1)
+        {
+            for(int j = 0; j < vertices.size(); j++)
+            {
+                if(vertices[j].name == v2 && i != j)
+                {
+                    adjVertex av;
+                    av.v = &vertices[j];
+                    av.weight = weight;
+                    vertices[i].adj.push_back(av);
+                }
+            }
+        }
+    }
 }
 
 void Travel::addCity(std::string cityName)
 {
+	bool found = false;
+    for(int i = 0; i < vertices.size(); i++)
+    {
+        if(vertices[i].name == cityName)
+        {
+            found = true;
+            cout<<vertices[i].name<<" found."<<endl;
+        }
+    }
+    if(found == false)
+    {
+        vertex v;
+        v.name = cityName;
+        v.districtID = 0;
+        vertices.push_back(v);
+        //cout << "Added: " << v.name << endl;
+    }
 
 }
 
 void Travel::addHub(std::string cityName)  //GET RID OF THIS ONE
 {
-
+	bool found = false;
+    for(int i = 0; i < hubCityVertices.size(); i++)
+    {
+        if(hubCityVertices[i].name == cityName)
+        {
+            found = true;
+            cout<<hubCityVertices[i].name<<" found."<<endl;
+        }
+    }
+    if(found == false)
+    {
+        vertex v;
+        v.name = cityName;
+        v.districtID = 0;
+       // v.isAirport = true;
+        hubCityVertices.push_back(v);
+        //cout << "Added: " << v.name << endl;
+    }
 }
 
 void Travel::addPlanePath(std::string v1, std::string v2, int weight)
 {
-
+	//SET V1, V2 AS PLANE PATHS
+    //find indexes
+    int v1Index;
+    int v2Index;
+ 
+    for(int i=0; i<hubCityVertices.size(); i++)
+    {
+        if(v1 == hubCityVertices[i].name)
+        {
+            v1Index=i;
+        }
+    }
+ 
+    for(int i=0; i<hubCityVertices.size(); i++)
+    {
+        if(v2 == hubCityVertices[i].name)
+        {
+            v2Index=i;
+        }
+    }
+ 
+    hubCityVertices[v1Index].isAirport = true;
+    hubCityVertices[v2Index].isAirport = true;
+ 
+        for(int i = 0; i < hubCityVertices.size(); i++)
+        {
+        //cout << hubCityVertices[i].name << endl;
+        if(hubCityVertices[i].name == v1)
+        {
+            for(int j = 0; j < hubCityVertices.size(); j++)
+            {
+                if(hubCityVertices[j].name == v2 && i != j)
+                {
+                    adjVertex av;
+                    av.v = &hubCityVertices[j];
+                    av.weight = weight;
+                    hubCityVertices[i].adj.push_back(av);
+                }
+            }
+        }
+    }
 }
 
 
