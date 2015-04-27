91903636d9f579921406f95eb36ca1213e198f3b
k <k@K>
2015-04-27 00:22:32 -0600

    Added menu functions to main.

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "Travel.h"
+
+using namespace std;
+
+void welcomeMenu()
+{
+    cout << "------------------------------" << endl;
+    cout << "Welcome to Travel Planner" << endl;
+    cout << "------------------------------" << endl;
+    cout << "To Begin, ";
+}
+
+void plannerMenu()
+{
+    cout << "" << endl;
+    cout << "   Please make a selection" << endl;
+    cout << "   1. Plan fastest route" << endl;
+    cout << "   2. Plan cheapest route" << endl;
+
+}
+
+void printMenu()
+{
+    cout <<  "Please Make A Selection" << endl;
+    cout << "------------------------------" << endl;
+    cout << "1. Print All City Options" << endl;
+    cout << "2. View Options for Ground Travel" << endl;
+    cout << "3. View Options for Air Travel" << endl;
+    cout << "4. Begin Planning Trip" << endl;
+    cout << "5. Quit" << endl;
+
+}
+
+int main()
+{
+
+
+}
+
